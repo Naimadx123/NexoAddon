de642d8f4a0be75b117e574f19b94cf992d98cfb
Naimadx123 <damianx787@gmail.com>
2025-10-02 18:18:31 +0200

    feat: add decay support for block place and nexo block events

--- src/main/java/zone/vao/nexoAddon/items/mechanics/Decay.java
+++ src/main/java/zone/vao/nexoAddon/items/mechanics/Decay.java
@@ -1,5 +1,7 @@
 package zone.vao.nexoAddon.items.mechanics;
 
+import com.nexomc.nexo.api.events.custom_block.NexoBlockBreakEvent;
+import com.nexomc.nexo.api.events.custom_block.NexoBlockPlaceEvent;
 import com.nexomc.nexo.api.events.custom_block.chorusblock.NexoChorusBlockBreakEvent;
 import com.nexomc.nexo.api.events.custom_block.noteblock.NexoNoteBlockBreakEvent;
 import com.nexomc.nexo.api.events.custom_block.stringblock.NexoStringBlockBreakEvent;
@@ -7,6 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import zone.vao.nexoAddon.utils.BlockUtil;
 
 import java.util.List;
@@ -14,22 +17,23 @@ import java.util.List;
 public record Decay(int time, double chance, List<Material> base, List<String> nexoBase, int radius) {
 
   public static class DecayListener implements Listener {
+
     @EventHandler
-    public void onNoteBlockBreak(NexoNoteBlockBreakEvent event) {
+    public void onNexoBlockPlace(NexoBlockPlaceEvent event) {
 
       BlockUtil.startDecay(event.getBlock().getLocation());
     }
 
     @EventHandler
-    public void onChorusBreak(NexoChorusBlockBreakEvent event) {
+    public void onBlockPlace(BlockPlaceEvent event) {
 
-      BlockAura.BlockAuraListener.onBlockBreak(event);
+      BlockUtil.startDecay(event.getBlock().getLocation());
     }
 
     @EventHandler
-    public void onStringBreak(NexoStringBlockBreakEvent event) {
+    public void onNexoBlockBreak(NexoBlockBreakEvent event) {
 
-      BlockAura.BlockAuraListener.onBlockBreak(event);
+      BlockUtil.startDecay(event.getBlock().getLocation());
     }
 
     @EventHandler
