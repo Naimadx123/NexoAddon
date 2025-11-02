d1412d4afd6cb2dc1bede62566f3ef238973e128
Naimadx123 <damianx787@gmail.com>
2025-11-02 15:04:21 +0100

    feat: add configurable chorus plant replacement mechanic

--- src/main/java/zone/vao/nexoAddon/events/chunk/ChunkLoadListener.java
+++ src/main/java/zone/vao/nexoAddon/events/chunk/ChunkLoadListener.java
@@ -3,6 +3,7 @@ package zone.vao.nexoAddon.events.chunk;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
+import zone.vao.nexoAddon.NexoAddon;
 
 public class ChunkLoadListener implements Listener {
 
@@ -11,5 +12,10 @@ public class ChunkLoadListener implements Listener {
 
     FurniturePopulator.onLoad(event);
     SaplingPopulator.onLoad(event);
+
+    if(NexoAddon.getInstance().getGlobalConfig().getBoolean("replace_chorus_plant", false)) {
+      ChorusPlantFixer.onLoad(event);
+    }
+
   }
 }
